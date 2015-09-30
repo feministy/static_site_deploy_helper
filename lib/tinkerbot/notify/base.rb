a4ac7ae657f3837ff9c31ec191140f71bc2690a2
Liz Abinante <eabinante@gmail.com>
2015-09-30 05:59:15 -0700

    notify extracted

---
+++ lib/tinkerbot/notify/base.rb
@@ -0,0 +1,26 @@
+# module Tinkerbot
+#   module Notify
+#     module Base
+#       extend self
+
+#       def help
+#         puts "#{'-' * 70}"
+#         puts "* ~ * ~ * TINKERBOT!! * ~ * ~ *"
+#         puts "oh hey, so you want to see how tinkerbot works?"
+#         puts ""
+#         puts ">> GETTING STARTED:"
+#         puts "run the following command in terminal:"
+#         puts "$ tinkerbot config"
+#         puts "and follow the on-screen prompts."
+#         puts ""
+#         puts ">> MORE HELP:"
+#         puts "more comprehensive documentation can be found on github:"
+#         puts "https://github.com/feministy/tinkerbot"
+#         puts ""
+#         puts ">> OPTIONS:"
+#         puts "all passed options are conveniently printed below."
+#         puts "#{'-' * 70}"
+#       end
+#     end
+#   end
+# end
